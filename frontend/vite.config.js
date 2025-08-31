4eba00cf00457a1b65b1a97871e93a5ba884232b
Pragathi <kodmadpragathi@gmail.com>
2025-08-31 22:12:26 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT || 8080, // Bind to Render's PORT
+    host: true                      // Allow external access
+  }
 })
